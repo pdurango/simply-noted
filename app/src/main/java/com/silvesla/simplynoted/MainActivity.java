bdc1a9571a26785df4ef54177e97404d694ee38c
pdurango <silvestri_21@hotmail.com>
2018-06-15 21:38:26 -0400

    Added gesture handlers

--- app/src/main/java/com/silvesla/simplynoted/MainActivity.java
+++ app/src/main/java/com/silvesla/simplynoted/MainActivity.java
@@ -2,7 +2,11 @@ package com.silvesla.simplynoted;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
@@ -22,6 +26,8 @@ public class MainActivity extends AppCompatActivity {
     ArrayAdapter<String> itemsAdapter;
     ListView listView;
 
+    private GestureDetector mDetector;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,10 +38,16 @@ public class MainActivity extends AppCompatActivity {
         readFile();
         itemsAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, itemsList);
         listView.setAdapter(itemsAdapter);
-
-        //itemsList.add("banana");
-        //itemsList.add("beee");
         listViewListener();
+
+        //mDetector = new GestureDetector(this, new MyGestureListener());
+        //listView.setOnTouchListener(touchListener);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN);
     }
 
     public void addNewItem(View view) {
@@ -46,17 +58,6 @@ public class MainActivity extends AppCompatActivity {
             itemsAdapter.add(item);
         addItem.setText("");
         writeFile();
-    }
-
-    private void listViewListener() {
-        listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
-            public boolean onItemLongClick(AdapterView<?> adapter, View view, int position, long id) {
-                itemsList.remove(position);
-                itemsAdapter.notifyDataSetChanged();
-                writeFile();
-                return true;
-            }
-        });
     }
 
     private void readFile() {
@@ -78,4 +79,64 @@ public class MainActivity extends AppCompatActivity {
             e.printStackTrace();
         }
     }
+
+
+    private void listViewListener() {
+        listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            public boolean onItemLongClick(AdapterView<?> adapter, View view, int position, long id) {
+                itemsList.remove(position);
+                itemsAdapter.notifyDataSetChanged();
+                writeFile();
+                return true;
+            }
+        });
+    }
+
+    View.OnTouchListener touchListener = new View.OnTouchListener() {
+        public boolean onTouch(View view, MotionEvent event) {
+        return mDetector.onTouchEvent(event);
+        }
+    };
+
+    class MyGestureListener extends GestureDetector.SimpleOnGestureListener {
+        @Override
+        public boolean onDown(MotionEvent event)
+        {
+            Log.d("TAG", "onDown: ");
+            return true;
+        }
+        @Override
+        public boolean onSingleTapConfirmed(MotionEvent e) {
+            Log.i("TAG", "onSingleTapConfirmed: ");
+            return true;
+        }
+
+        @Override
+        public void onLongPress(MotionEvent e) {
+            Log.i("TAG", "onLongPress: ");
+        }
+
+        @Override
+        public boolean onDoubleTap(MotionEvent e) {
+            Log.i("TAG", "onDoubleTap: ");
+            return true;
+        }
+
+        @Override
+        public boolean onScroll(MotionEvent e1, MotionEvent e2,
+                                float distanceX, float distanceY) {
+            Log.i("TAG", "onScroll: ");
+            return true;
+        }
+
+        @Override
+        public boolean onFling(MotionEvent event1, MotionEvent event2,
+                               float velocityX, float velocityY) {
+            Log.d("TAG", "onFling: ");
+            return true;
+        }
+    }
 }
+
+//https://stackoverflow.com/questions/45054908/how-to-add-a-gesture-detector-to-a-view-in-android
+
