0f7fb0af6485439716cf3f463771621505e5b6ec
pdurango <silvestri_21@hotmail.com>
2018-06-19 21:53:44 -0400

    Added sub-item functionality - Only works hardcoded

--- app/src/main/java/com/silvesla/simplynoted/MainActivity.java
+++ app/src/main/java/com/silvesla/simplynoted/MainActivity.java
@@ -11,17 +11,94 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
+import android.widget.SimpleAdapter;
 import android.widget.TextView;
 
 import org.apache.commons.io.FileUtils;
 
 import java.io.File;
 import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
+    ListView listView;
+    List<HashMap<String, String>> itemsList;
+    SimpleAdapter itemsAdapter;
+    HashMap<String, String> nameAddress;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        listView = (ListView) findViewById(R.id.task_listview);
+
+        DateFormat dateFormat = new SimpleDateFormat("dd MM yyyy");
+        String curDate = dateFormat.format(new Date());
+
+
+        nameAddress = new HashMap<>();
+
+        nameAddress.put("Bill", "1212");
+        nameAddress.put("Dillon", "2123");
+        nameAddress.put("Jodie", "5212");
+
+        itemsList = new ArrayList<>();
+        itemsAdapter = new SimpleAdapter(this, itemsList, R.layout.list_item, new String[]{"First Line", "Second Line"}, new int[]{R.id.text1, R.id.text2});
+        Iterator iterator = nameAddress.entrySet().iterator();
+        listView.setAdapter(itemsAdapter);
+        listViewListener();
+
+
+        while (iterator.hasNext())
+        {
+            HashMap<String, String> resultsMap = new HashMap<>();
+            Map.Entry pair = (Map.Entry)iterator.next();
+            resultsMap.put("First Line", pair.getKey().toString());
+            resultsMap.put("Second Line", pair.getValue().toString());
+            itemsList.add(resultsMap);
+        }
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN);
+    }
+
+    public void addNewItem(View view) {
+       /* TextView addItem = (TextView) findViewById(R.id.task_textview);
+        String item = addItem.getText().toString();
+        if (item.equals("") || item.equals("Add a todo")) ;
+        else
+            itemsAdapter.add(item);
+        addItem.setText(""); */
+    }
+
+
+
+    private void listViewListener() {
+        listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            public boolean onItemLongClick(AdapterView<?> adapter, View view, int position, long id) {
+                itemsList.remove(position);
+                itemsAdapter.notifyDataSetChanged();
+                return true;
+            }
+        });
+    }
+
+}
+
+
+    /*
     ArrayList<String> itemsList;
     ArrayAdapter<String> itemsAdapter;
     ListView listView;
@@ -137,6 +214,6 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 }
-
+*/
 //https://stackoverflow.com/questions/45054908/how-to-add-a-gesture-detector-to-a-view-in-android
 
